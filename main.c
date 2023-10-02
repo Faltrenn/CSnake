2c537a55519e54cfc8ad0818b48fcb6c3ccebbb7
Emanuel Alves <emanuelalvesp5@gmail.com>
2023-10-01 21:54:58 -0300

    Adição do spawn de maçãs. Adição da pontuação.

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define INFO_WIDTH 10
+#define INFO_HEIGHT LINES
+#define GAME_WIDTH (COLS - INFO_WIDTH)
+#define GAME_HEIGHT LINES
+
 typedef struct {
     unsigned int x;
     unsigned int y;
@@ -38,11 +43,19 @@ SnakeBody * get_snake_head(SnakeBody *snake) {
     return NULL;
 }
 
+Vector2 generate_apple() {
+    Vector2 apple;
+    apple.x = rand() % GAME_WIDTH;
+    apple.y = rand() % GAME_HEIGHT;
+
+    return apple;
+}
+
 int main() {
     initscr();
     cbreak();
     noecho();
-    curs_set(0);
+    curs_set(0); 
 
     nodelay(stdscr, 1);
 
@@ -56,8 +69,25 @@ int main() {
 
     SnakeBody *snake = create_snake_body(position, move_direction, NULL);
 
+    WINDOW *left_window = newwin(INFO_HEIGHT, INFO_WIDTH, 0, 0);
+    WINDOW *game_window = newwin(GAME_HEIGHT, GAME_WIDTH, 0, INFO_WIDTH);
+
+    int score = 0;
+
+    
+    refresh();
+
+    wclear(left_window);
+    mvwprintw(left_window, 0, 0, "Score: %d", score);
+    wrefresh(left_window);
+
+    Vector2 apple = generate_apple();
+
+
+
     while(1) {
-        clear();
+        wclear(game_window);
+
         for(SnakeBody *s = snake; s != NULL; s = s->next) {
             Vector2 p = s->position;
             p.x += s->move_direction.x;
@@ -67,16 +97,28 @@ int main() {
                 s->move_direction = s->next->move_direction;
             }
 
-            mvprintw(s->position.y, s->position.x, "0");
+            mvwprintw(game_window, s->position.y, s->position.x, "0");
         }
-        refresh();
+
+        Vector2 head_position = get_snake_head(snake)->position;
+        if(head_position.x == apple.x && head_position.y == apple.y) {
+            apple = generate_apple();
+            snake = create_snake_body(snake->position, move_direction, snake);
+            
+            score++;
+            wclear(left_window);
+            mvwprintw(left_window, 0, 0, "Score: %d", score);
+            wrefresh(left_window);
+        }
+
+        mvwprintw(game_window, apple.y, apple.x, "A");
+        
+        wrefresh(game_window);
 
         int key = getch();
         if(key != ERR) {
             Vector2 new_direction;
-            if(key == 'e') {
-                snake = create_snake_body(snake->position, move_direction, snake);
-            } else if(key == 'd') {
+            if(key == 'd') {
                 new_direction.x = 1;
                 new_direction.y = 0;
                 get_snake_head(snake)->move_direction = new_direction;
